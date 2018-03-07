aef61b32dbcfa17636062787955a98cf7652295b
Kyle Corry <kylecorry31@gmail.com>
2018-03-07 13:42:13 -0500

    Update about page

--- src/main/java/com/emeraldElves/alcohollabelproject/ui/controllers/AboutPageController.java
+++ src/main/java/com/emeraldElves/alcohollabelproject/ui/controllers/AboutPageController.java
@@ -1,8 +1,23 @@
 package com.emeraldElves.alcohollabelproject.ui.controllers;
 
+import com.jfoenix.controls.JFXScrollPane;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ScrollPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 /**
  * Created by Dan on 4/22/2017.
  */
-public class AboutPageController{
+public class AboutPageController implements Initializable {
 
+    @FXML
+    private ScrollPane scrollPane;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        JFXScrollPane.smoothScrolling(scrollPane);
+    }
 }
